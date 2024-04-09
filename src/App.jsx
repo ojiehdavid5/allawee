26677b6481fb2e133d6a9708739ff9ce6029cb60
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-09 09:33:24 +0100

    changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function App() {
+
+  return (
+    <div className='bg-[#0D1F23]'>
+      <h1 className='text-4xl font-bold'>chuks
+
+      </h1>
+    </div>
+  )
+}
+
+export default App
