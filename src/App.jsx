2bd5b4f38e9befd95cca673815eb98c25039de16
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-18 15:22:32 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import {useEffect} from 'react'
+import Nav from "./components/Nav"
+import Hero from "./components/Hero"
+import Info from "./components/Info"
 function App() {
   // useEffect(()=>{
   //   AOS.init();
   // })
 
   return (
-    <div className='bg-[rgb(13,31,35)]'>
+    <div className='mx-[5rem]'>
+      <Nav/>
+      <Hero/>
+    <Info/>
       
       
     </div>
