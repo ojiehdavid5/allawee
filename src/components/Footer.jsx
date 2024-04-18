7cfaad77a7d78c02514a14362e230b470e0615f1
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-18 18:19:18 +0100

    changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex flex-row  '>
+        <div className='flex flex-col '>
+
+        </div>
+        <div className='flex flex-col'></div>
+        <div className='flex flex-col'></div>
+      
+    </div>
+  )
+}
+
+export default Footer
