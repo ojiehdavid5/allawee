d7a2da778f8fda4648e0e23d4d38ea6e9f4b89bf
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-18 15:23:42 +0100

    changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Logo from '../assets/logo.svg'
 
 const Nav = () => {
   return (
-    <div>
+    <header className='w-full h-[6rem] flex justify-center items-center'>
+      <nav className='w-full h-16 flex flex-row justify-between  items-center'>
+        <h1 className='text-2xl text-white ml-1'>
+          Blog
+        </h1>
+        <div>
+          <img src={Logo} alt='logo'/>
+
+        </div>
+
+        <h1 className='text-2xl text-white mr-5'>
+          Career
+          </h1>
+
+
+      </nav>
       
-    </div>
+    </header>
   )
 }
 
