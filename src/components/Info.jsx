1dd9ec832f469600fee52dabc557fac5cae62393
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-18 15:23:31 +0100

    changes

---
+++ src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='mt-[20rem] min-h-screen'>
+      <h1 className='text-white'>Hello World</h1>
+
+    </div>
+  )
+}
+
+export default Info
