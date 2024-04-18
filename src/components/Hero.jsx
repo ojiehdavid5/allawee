337f33f6c9393609b0e0bff551cc6411a83abfbe
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-18 15:23:21 +0100

    changes

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='mt-[12rem] flex flex-col  space-x-10 items-center justify-between text-white min-h-[screen]'>
+        <h1 className='text-[#A1DDC2] text-2xl text-bold mb-7' >Allawee is a financial technology platform built with <span className='text-white'>YOU</span> in mind.
+
+</h1>
+<h2 className='text-5xl font-bold mb-7'>Your Money. Your Way.
+</h2>
+<h1 className=' text-2xl w-[50rem] mb-7'>Whether you are an individual or business, we can help you put super controls on your money using technology.
+
+</h1>
+
+<h2 className='mb-7 text-2xl'>We believe in freedom & happiness!</h2>
+
+
+
+
+      
+    </div>
+  )
+}
+
+export default Hero
