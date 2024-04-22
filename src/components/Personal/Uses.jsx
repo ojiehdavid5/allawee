91d36f706ad490f8c37e2990abd415ec8b3625be
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-22 17:10:01 +0100

    changes

--- src/components/Personal/Uses.jsx
+++ src/components/Personal/Uses.jsx
@@ -6,10 +6,48 @@ import half from '../../assets/personal/half.png'
 import Googleplay2 from '../../assets/personal/Googleplay2.png'
 import Appstore2 from '../../assets/personal/Appstore2.png'
 
+import Skate from '../../assets/personal/skateboy.png'
+import icecream from '../../assets/personal/icecream.png'
+import eee from '../../assets/personal/eee.png'
+import card1 from '../../assets/personal/card1.png'
+import card2 from '../../assets/personal/card2.png'
+import coin from '../../assets/personal/coin.png'
+import handcard from '../../assets/personal/handcard.png'
+import money from '../../assets/personal/money.png'
+
 const Uses = () => {
   return (
     <div className='mt-[25rem] flex flex-col justify-center items-center '>
-        <div className=" h-[790px] w-[624px] bg-white flex flex-col items-center rounded-lg p-7">
+    <img src={Skate}
+    className=' aspect-[208/48]  max-w-full h-full object-cover box-border overflow-hidden p-0 mt-0 z-[99] absolute -bottom-[162rem] w-[30rem] left-[20rem] '
+     alt='skate'/>
+    <img src={icecream}
+    className=' aspect-[208/48]  max-w-full h-[25rem] object-cover box-border overflow-hidden p-0 mt-0 z-[99] absolute -bottom-[143rem] w-[15rem]  left-[45rem] flex justify-center items-center '
+     alt='skate'/>
+    <img src={eee}
+    className=' aspect-[208/48]  max-w-full h-[10rem] object-cover box-border overflow-hidden p-0 mt-0 z-[99] absolute -bottom-[150rem] w-[10rem]  left-[70rem]  flex justify-center items-center '
+     alt='skate'/>
+    <img src={card1}
+    className=' aspect-[208/48]  max-w-full h-[15rem] object-cover box-border overflow-hidden p-0 mt-0 z-[99] absolute -bottom-[180rem] w-[11rem]  left-[70rem]  flex justify-center items-center '
+     alt='skate'/>
+    <img src={coin}
+    className=' aspect-[208/48]  max-w-full h-[12rem] object-cover box-border overflow-hidden p-0 mt-0 z-[99] absolute -bottom-[190rem] w-[11rem]  left-[55rem]  flex justify-center items-center '
+     alt='skate'/>
+    <img src={card2}
+    className=' aspect-[208/48]  max-w-full h-[14rem] object-cover box-border overflow-hidden p-0 mt-0 z-[99] absolute -bottom-[220rem] w-[12rem]  right-[65rem]  flex justify-center items-center '
+     alt='skate'/>
+    <img src={money}
+    className=' aspect-[208/48]  max-w-full h-[14rem] object-cover box-border overflow-hidden p-0 mt-0 z-[99] absolute -bottom-[240rem] w-[15rem]  left-[65rem]  flex justify-center items-center '
+     alt='skate'/>
+         <img src={coin}
+    className=' aspect-[208/48]  max-w-full h-[12rem] object-cover box-border overflow-hidden p-0 mt-0 z-[99] absolute -bottom-[250rem] w-[11rem]  left-[55rem]  flex justify-center items-center '
+     alt='skate'/>
+         <img src={handcard}
+    className=' aspect-[208/48]  max-w-full h-[17rem] object-cover box-border overflow-hidden p-0 mt-0 z-[99] absolute -bottom-[250rem] w-[18rem]  right-[65rem]  flex justify-center items-center '
+     alt='skate'/>
+
+
+        <div className=" h-[790px] w-[624px] bg-white flex flex-col items-center rounded-lg p-7 z-0">
           <img src={personal}
           className='w-[48vh] mt-[3rem] bg-[#F0FDE8]  box-border'
           alt='personal'/>
