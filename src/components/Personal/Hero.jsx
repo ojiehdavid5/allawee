8c1ffde6c760eb96dc864bfdd78c5e071c9bd258
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-22 03:24:07 +0100

    changes

--- src/components/Personal/Hero.jsx
+++ src/components/Personal/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <p className='text-2xl text-white leading-7'>SPEND LIKE A LOCAL, LIVE LIKE A TOURIST.
 
 </p>
-<h1 className='mt-5 font-bold text-[10rem] tracking-tighter hover:tracking-wide text-[#DBFAC8]'>YOUR MONEY.</h1>
+<h1 className='mt-5 font-bold text-[10rem] tracking-[-1rem] hover:tracking-wide text-[#DBFAC8] font-bebas-neue'>YOUR MONEY.</h1>
 <h1 className='mt-5 font-bold text-[10rem] tracking-tighter hover:tracking-wide text-[#DBFAC8]'>YOUR WAY.</h1>
     </div>
   )
