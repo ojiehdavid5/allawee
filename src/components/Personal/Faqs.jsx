a6d920cb93ea84b46447e8c51a3b787254be8f56
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-23 20:01:13 +0100

    changes

--- src/components/Personal/Faqs.jsx
+++ src/components/Personal/Faqs.jsx
@@ -5,7 +5,13 @@ const Faqs = () => {
     <div className='mt-[10rem]'>
         <h1 className='text-5xl font-medium ml-[4rem] text-white'> FAQS</h1>
         <div className='flex flex-col justify-center items-center'>
+
+
+
+
+          
           <div> 
+
              
           </div>
 
