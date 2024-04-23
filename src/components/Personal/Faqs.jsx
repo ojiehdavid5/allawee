2a3c1b8e65b6cf1f53d57371e9633d3929cc7b14
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-23 13:11:45 +0100

    changes

---
+++ src/components/Personal/Faqs.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Faqs = () => {
+  return (
+    <div className='mt-[10rem]'>
+        <h1 className='text-5xl font-medium ml-[4rem] text-white'> FAQS</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <div> 
+             
+          </div>
+
+        </div>
+
+
+
+      
+    </div>
+  )
+}
+
+export default Faqs
