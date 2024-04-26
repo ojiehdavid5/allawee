c3ad4508e7ad43e655e3dff91e7fb1b8da53602c
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-26 14:28:53 +0100

    working on carousel

--- src/components/Personal/faq.jsx
+++ src/components/Personal/faq.jsx
@@ -2,29 +2,33 @@ import React, { useState } from 'react';
 import cross from '../../assets/personal/cross.svg';
 
 const Faq = () => {
-  const [active, setActive] = useState(false);
-  console.log(active);
+  const [active1, setActive1] = useState(false);
+  const [active2, setActive2] = useState(false);
+  const [active3, setActive3] = useState(false);
+  const [active4, setActive4] = useState(false);
+  const [active5, setActive5] = useState(false);
+  const [active6, setActive6] = useState(false);
 
   return (
     <div className='flex flex-col min-h-screen mt-[10rem] justify-center items-center'>
-      <div className={active ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
+      <div className={active1 ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
         <div className={"flex flex-col justify-center items-start space-x-2 space-y-4 h-[20rem] w-[60rem]"}>
                   <p className='text-2xl font-medium ml-[6rem] text-white'>How do I signup? Do I need a bank account?</p>
-<p className={active?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center':'hidden'}>Download the Allawee app on the iOS App Store or Google Play Store and follow the on-screen instructions to create your account. All you need is your phone number to create your account. Government issued ID, BVN & Selfie will be required to increase account limits.</p>
+<p className={active1?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center':'hidden'}>Download the Allawee app on the iOS App Store or Google Play Store and follow the on-screen instructions to create your account. All you need is your phone number to create your account. Government issued ID, BVN & Selfie will be required to increase account limits.</p>
         </div>
 
 
         <img
           src={cross}
-          onClick={() => setActive(true)}
-          className='w-[28px] h-[28px] mr-[6rem] -mt-[3rem] bg-white'
+          onClick={() => setActive1(true)}
+          className='w-[28px] h-[28px] mr-[6rem] -mt-[3rem] bg-white bg-clip-white'
           alt=''
         />
       </div>
-      <div className={active ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
+      <div className={active2 ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
         <div className={"flex flex-col justify-center items-start space-x-2 space-y-4 h-[20rem] w-[60rem]"}>
                   <p className='text-2xl font-medium ml-[6rem] text-white'>Do I need to have a bank account?</p>
-<p className={active?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center':'hidden'}>No, you don't need a bank account to create an Allawee account or add money to your balance.
+<p className={active2?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center':'hidden'}>No, you don't need a bank account to create an Allawee account or add money to your balance.
 
 All account approvals are subject to CBN KYC & AML/CFT regulations & guidelines.</p>
         </div>
@@ -32,15 +36,15 @@ All account approvals are subject to CBN KYC & AML/CFT regulations & guidelines.
 
         <img
           src={cross}
-          onClick={() => setActive(true)}
+          onClick={() => setActive2(true)}
           className='w-[28px] h-[28px] mr-[6rem] -mt-[3rem] bg-white'
           alt=''
         />
       </div>
-      <div className={active ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
+      <div className={active3 ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
         <div className={"flex flex-col justify-center items-start space-x-2 space-y-4 h-[20rem] w-[60rem]"}>
                   <p className='text-2xl font-medium ml-[6rem] text-white'>How does the card work?</p>
-<p className={active?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center flex-wrap flex':'hidden'}>Easily create as many physical or virtual cards as you wish & they will be delivered to you.
+<p className={active3?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center flex-wrap flex':'hidden'}>Easily create as many physical or virtual cards as you wish & they will be delivered to you.
 
 Simply request a card from our options of Mastercard or Verve & setup your 4 digit PIN once it arrives. You can create cards for loved ones with your own special controls and limits too.
 
@@ -52,29 +56,29 @@ CVV is the 3 digit number at the back of your card. Happy Spending!</p>
 
         <img
           src={cross}
-          onClick={() => setActive(true)}
+          onClick={() => setActive3(true)}
           className='w-[28px] h-[28px] mr-[6rem] -mt-[3rem] bg-white'
           alt=''
         />
       </div>
-      <div className={active ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
+      <div className={active4 ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
         <div className={"flex flex-col justify-center items-start space-x-2 space-y-4 h-[20rem] w-[60rem]"}>
                   <p className='text-2xl font-medium ml-[6rem] text-white'>How do I load or receive money?</p>
-<p className={active?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center':'hidden'}>You get a dedicated bank account number for yourself instantly, where you can transfer funds into your account seamlessly. You can also top up with a card, which attracts a 1.5% fee capped at N2,000. Click on the 'Add Money' button on the homepage to load your account.</p>
+<p className={active4?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center':'hidden'}>You get a dedicated bank account number for yourself instantly, where you can transfer funds into your account seamlessly. You can also top up with a card, which attracts a 1.5% fee capped at N2,000. Click on the 'Add Money' button on the homepage to load your account.</p>
         </div>
 
 
         <img
           src={cross}
-          onClick={() => setActive(true)}
+          onClick={() => setActive4(true)}
           className='w-[28px] h-[28px] mr-[6rem] -mt-[3rem] bg-white'
           alt=''
         />
       </div>
-      <div className={active ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
+      <div className={active5 ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
         <div className={"flex flex-col justify-center items-start space-x-2 space-y-4 h-[20rem] w-[60rem]"}>
                   <p className='text-2xl font-medium ml-[6rem] text-white'>How do I spend my money?</p>
-<p className={active?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center':'hidden'}>You can either use your allawee card to pay anywhere! POS, Online or ATM.
+<p className={active5?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center':'hidden'}>You can either use your allawee card to pay anywhere! POS, Online or ATM.
 
 Instantly transfer money to any bank account or wallet in Nigeria. Get instant push notifications & emails immediately a transaction happens.</p>
         </div>
@@ -82,15 +86,15 @@ Instantly transfer money to any bank account or wallet in Nigeria. Get instant p
 
         <img
           src={cross}
-          onClick={() => setActive(true)}
+          onClick={() => setActive5(true)}
           className='w-[28px] h-[28px] mr-[6rem] -mt-[3rem] bg-white'
           alt=''
         />
       </div>
-      <div className={active ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
+      <div className={active6 ? 'w-[80rem] h-[18rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]' : 'w-[80rem] h-[12rem] mx-10 rounded-md flex flex-row items-center justify-between bg-[#161B39] border border-[#48535C] mb-[5rem]'}>
         <div className={"flex flex-col justify-center items-start space-x-2 space-y-4 h-[20rem] w-[60rem]"}>
-                  <p className='text-2xl font-medium ml-[6rem] text-white'>How does Allawee keep my money safe?</p>
-<p className={active?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center':'hidden'}>We use advanced encryption and fraud detection technology to ensure the security of your data and finances. Any information you provide is encrypted and securely transmitted to our servers.
+                  <p className='text-2xl font-medium ml-[6rem] text-white'> How does Allawee keep my money safe?</p>
+<p className={active6?'text-1xl w-[40rem] h-[10rem] text-white  p-5 mr-[12rem] leading-8  text-center':'hidden'}>We use advanced encryption and fraud detection technology to ensure the security of your data and finances. Any information you provide is encrypted and securely transmitted to our servers.
 
 Funds are safely held with Providus Bank PLC, a CBN licensed commercial bank. Debit & prepaid cards are issued by Providus Bank PLC pursuant to license by Mastercard & Verve.</p>
         </div>
@@ -98,7 +102,7 @@ Funds are safely held with Providus Bank PLC, a CBN licensed commercial bank. De
 
         <img
           src={cross}
-          onClick={() => setActive(true)}
+          onClick={() => setActive6(true)}
           className='w-[28px] h-[28px] mr-[6rem] -mt-[3rem] bg-white'
           alt=''
         />
