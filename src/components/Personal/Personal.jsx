dd56bba5bd331331adcbacf4d6ef2c57b7412536
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-28 17:21:34 +0100

    Responsiveness

--- src/components/Personal/Personal.jsx
+++ src/components/Personal/Personal.jsx
@@ -14,7 +14,7 @@ import Footer from './Footer'
 import Slide from './Slide'
 const Personal = () => {
   return (
-    <div className='mx-10 bg-[#0B1032] mih-h-screen'>
+    <div className='mx-10 bg-[#0D1F23] mih-h-screen'>
         <Nav/>
 <Hero/>
 <App/>
