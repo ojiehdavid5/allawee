71f423fb69ffcaacef0a005744eaad2b9e9200a7
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-02 15:02:33 +0100

    responsiveness

--- src/components/Home/Bottom.jsx
+++ src/components/Home/Bottom.jsx
@@ -6,7 +6,7 @@ const Bottom = () => {
   return (
     <div className='w-full max-lg:ml-[3rem]'>
         <img src={allawee}
-        className=' max-lg:w-[20rem] w-[50rem]'
+        className=' max-lg:w-[20rem] w-full'
          alt='allawee'/>
       
     </div>
