fdcfab60677a402d98dd77d79be4f49e292d0075
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-18 06:26:05 +0100

    CHANGES

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='mt-[10rem] max-lg:mb-[20rem] flex flex-col   items-center justify-between text-white  max-lg:w-[20rem] max-lg:h-[10rem]   '>
-        <h1 className='text-[#A1DDC2]  text-bold mb-7 max-lg:text-2max-lg max-lg:w-[20rem]  text-max-lg text-center max-lg:ml-[4rem]' >Allawee is a financial technology platform built with <span className='text-white'>YOU</span> in mind.
+        <h1 className='text-[#A1DDC2]  text-bold mb-7 max-lg:text-2xl max-lg:w-[20rem]  text-2xl text-center max-lg:ml-[4rem]' >Allawee is a financial technology platform built with <span className='text-white'>YOU</span> in mind.
 
 </h1>
-<h2 className='text-5max-lg font-bold mb-7 text-center max-lg:ml-[6rem] max-lg:w-[20rem] '>Your Money. Your Way.
+<h2 className='text-5xl font-bold mb-7 text-center max-lg:ml-[6rem] max-lg:w-[20rem] '>Your Money. Your Way.
 </h2>
-<h1 className=' text-2max-lg w-[22rem] mb-7 text-center max-lg:ml-[6rem]'>
+<h1 className=' text-2xl w-[22rem] mb-7 text-center max-lg:ml-[6rem]'>
   Whether you are an individual or business, we can help you put super controls on your money using technology.
 
 </h1>
 
-<h2 className='mb-7 text-2max-lg max-lg:ml-[6rem]  text-center  max-lg:w-[20rem] max-lg:mt-[2rem]'>We believe in freedom & happiness!</h2>
+<h2 className='mb-7 text-2xl max-lg:ml-[6rem]  text-center  max-lg:w-[20rem] max-lg:mt-[2rem]'>We believe in freedom & happiness!</h2>
 
 
 
