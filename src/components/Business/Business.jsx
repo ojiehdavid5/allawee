7e4ef30ed13c11c7dad8163f72568534ceb95b2a
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-21 19:11:53 +0100

    changes

---
+++ src/components/Business/Business.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Nav from './Nav'
+
+import Hero from './Hero'
+ import  About from './About'
+
+const Business = () => {
+  return (
+    <div>
+        <Nav/>
+        <Hero/>
+        
+        <About/>
+
+
+
+      
+    </div>
+  )
+}
+
+export default Business
--- src/components/Business/Nav.jsx
+++ src/components/Business/Nav.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Logo from '../../assets/Business/logo.svg'
 
 const Nav = () => {
   return (
-    <div>
-      <h1>changes</h1>
+    <div className='h-[7rem] w-full flex justify-center items-center'>
+      <div className="flex flex-row justify-between items-center  w-[60rem] h-[4rem] gap-[6rem]">
+        <img src={Logo} className=''/>
+        <div className=" flex flex-row justify-between items-center  gap-[8rem]">
+        <p >Features</p>
+        <p>About</p>
+        <p>Pricing</p>
+        <p>Blog</p>
+
+        </div>
+
+        <div className="flex flex-row justify-between items-center">
+          <button>Log in</button>
+          <button>create an account</button>
+        </div>
+
+
+
+      </div>
+
 
 
       
