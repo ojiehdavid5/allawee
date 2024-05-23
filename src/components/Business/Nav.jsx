4ba71300a87ea6613b8c794c79f84a73bb882da9
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-23 01:05:52 +0100

    changes

--- src/components/Business/Nav.jsx
+++ src/components/Business/Nav.jsx
@@ -3,9 +3,9 @@ import Logo from '../../assets/Business/logo.svg'
 
 const Nav = () => {
   return (
-    <div className='h-[7rem] flex justify-center items-center bg-[#0000]'>
+    <div className='h-[7rem] flex justify-center items-center bg-[#000] '>
       <div className="flex flex-row justify-between items-center   gap-[6rem]  bg-[#0000]">
-        <img src={Logo} className=''/>
+        <img src={Logo} className='w-[15%]'/>
         <div className=" flex flex-row justify-between items-center  gap-[3rem] text-[#ffff]">
         <p >Features</p>
         <p>About</p>
