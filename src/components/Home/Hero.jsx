1838e35381eae247beec2ed6e05a38d48b88e1df
ojieh (chuks) david <ojiehdavid5@gmail.com>
2024-10-29 18:51:30 +0100

    Update Hero.jsx

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <h1 className='text-[#A1DDC2]  text-bold mb-7 max-lg:text-2xl max-lg:w-[20rem]  text-2xl text-center max-lg:ml-[4rem]' >Allawee is a financial technology platform built with <span className='text-white'>YOU</span> in mind.
 
 </h1>
-<h2 className='text-5xl font-bold mb-7 text-center max-lg:ml-[6rem] max-lg:w-[20rem] '>Your Money. Your Way.
+<h2 className='text-4xl font-bold mb-7 text-center max-lg:ml-[6rem] max-lg:w-[20rem] '>Your Money. Your Way.
 </h2>
 <h1 className=' text-2xl w-[22rem] mb-7 text-center max-lg:ml-[6rem]'>
   Whether you are an individual or business, we can help you put super controls on your money using technology.
